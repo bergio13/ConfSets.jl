aa5016a98c57d43ebc5b639d9de4c98c9ac8360f
Giochelavaipiatti <78697256+bergio13@users.noreply.github.com>
2024-02-09 13:38:23 +0100

    added no sequential functionality

--- src/ConfSets.jl
+++ src/ConfSets.jl
@@ -23,14 +23,26 @@ function clt_std_margin(t::Vector, alpha::Float64)
     return quantile(Normal(), 1 - alpha / 2) ./ (t .^ 0.5)
 end
 
+function clt_std_margin(t::Real, alpha::Float64)
+    return quantile(Normal(), 1 - alpha / 2) / (t^0.5)
+end
+
 # Hoeffding margin
 function hoeff_margin(t::Vector, alpha::Float64, rang::Union{Float64,Int})
     return (((rang ./ (2 .* t)) .* log(2 / alpha)) .^ 0.5)
 end
 
+function hoeff_margin(t::Real, alpha::Float64, rang::Union{Float64,Int})
+    return (((rang / (2 * t)) * log(2 / alpha))^0.5)
+end
+
 # Chebyshev margin
 function cheb_margin(t::Vector, alpha::Float64)
     return (1 ./ ((t .* alpha) .^ 0.5))
+end
+
+function cheb_margin(t::Real, alpha::Float64)
+    return (1 / ((t * alpha)^0.5))
 end
 
 # Asympt Conf Sequence margin
@@ -40,60 +52,86 @@ function ACS_margin(t::Vector, alpha::Float64)
     return 1.7 .* nom ./ denom
 end
 
+function ACS_margin(t::Real, alpha::Float64)
+    nom = ((log(log(2 * t)) + 0.72 * log(10.4 / alpha))^0.5)
+    denom = t^0.5
+    return 1.7 * nom / denom
+end
+
 # This function computes the confidence intervals for the mean of a vector x, using the Central Limit Theorem.
-function clt_confidence_interval(x::Vector, alpha::Float64)
-    t = collect(1:length(x))
-    mu_hat = cumul_mean(x)
-    std = cumul_var(x) .^ 0.5
-    margin = clt_std_margin(t, alpha)
-    std_margin = std .* margin
-
-    std_margin[isnan.(std_margin)] .= Inf
-
-    return Dict("l" => mu_hat .- std_margin, "u" => mu_hat .+ std_margin)
-
+function clt_confidence_interval(x::Vector, alpha::Float64, sequential::Bool)
+    if sequential
+        t = collect(1:length(x))
+        mu_hat = cumul_mean(x)
+        st_dev = cumul_var(x) .^ 0.5
+        margin = clt_std_margin(t, alpha)
+        std_margin = st_dev .* margin
+        std_margin[isnan.(std_margin)] .= Inf
+        return Dict("l" => mu_hat .- std_margin, "u" => mu_hat .+ std_margin)
+    else
+        t = length(x)
+        mu_hat = mean(x)
+        st_dev = std(x)
+        std_margin = clt_std_margin(t, alpha) * st_dev
+        return Dict("l" => mu_hat - std_margin, "u" => mu_hat + std_margin)
+    end
 end
 
 # This function computes the confidence intervals for the mean of a vector x, using the Hoeffding inequality.
-function hoeff_confidence_interval(x::Vector, alpha::Float64, rang::Union{Float64,Int,Nothing})
-    t = collect(1:length(x))
-    mu_hat = cumul_mean(x)
+function hoeff_confidence_interval(x::Vector, alpha::Float64, rang::Union{Float64,Int,Nothing}, sequential::Bool)
     if isnothing(rang)
         rang = maximum(x) - minimum(x)
     end
-    margin = hoeff_margin(t, alpha, rang)
 
-    margin[isnan.(margin)] .= Inf
-
-    return Dict("l" => mu_hat .- margin, "u" => mu_hat .+ margin)
+    if sequential
+        t = collect(1:length(x))
+        mu_hat = cumul_mean(x)
+        margin = hoeff_margin(t, alpha, rang)
+        margin[isnan.(margin)] .= Inf
+        return Dict("l" => mu_hat .- margin, "u" => mu_hat .+ margin)
+    else
+        t = length(x)
+        mu_hat = mean(x)
+        margin = hoeff_margin(t, alpha, rang)
+        return Dict("l" => mu_hat - margin, "u" => mu_hat + margin)
+    end
 
 end
 
 # This function computes the confidence intervals for the mean of a vector x, using the Chebyshev inequality.
-function cheb_confidence_interval(x::Vector, alpha::Float64)
-    t = collect(1:length(x))
-    mu_hat = cumul_mean(x)
-    std = cumul_var(x) .^ 0.5
-    margin = cheb_margin(t, alpha) .* std
-
-    margin[isnan.(margin)] .= Inf
-
-    return Dict("l" => mu_hat .- margin, "u" => mu_hat .+ margin)
-
+function cheb_confidence_interval(x::Vector, alpha::Float64, sequential::Bool)
+    if sequential
+        t = collect(1:length(x))
+        mu_hat = cumul_mean(x)
+        st_dev = cumul_var(x) .^ 0.5
+        margin = cheb_margin(t, alpha) .* st_dev
+        margin[isnan.(margin)] .= Inf
+        return Dict("l" => mu_hat .- margin, "u" => mu_hat .+ margin)
+    else
+        t = length(x)
+        mu_hat = mean(x)
+        st_dev = std(x)
+        margin = cheb_margin(t, alpha) * st_dev
+        return Dict("l" => mu_hat - margin, "u" => mu_hat + margin)
+    end
 end
 
 # This function computes the confidence sets for the mean of a vector x, using the Asymptotic Confidence Sequence.
-function Asymp_conf_seq(x::Vector, alpha::Float64)
-    t = collect(1:length(x))
-    mu_hat = cumul_mean(x)
-    std = cumul_var(x) .^ 0.5
-
-    margin = ACS_margin(t, alpha) .* std
-
-    margin[isnan.(margin)] .= Inf
-
-    return Dict("l" => mu_hat .- margin, "u" => mu_hat .+ margin)
-
+function Asymp_conf_seq(x::Vector, alpha::Float64, sequential::Bool)
+    if sequential
+        t = collect(1:length(x))
+        mu_hat = cumul_mean(x)
+        st_dev = cumul_var(x) .^ 0.5
+        margin = ACS_margin(t, alpha) .* st_dev
+        margin[isnan.(margin)] .= Inf
+        return Dict("l" => mu_hat .- margin, "u" => mu_hat .+ margin)
+    else
+        t = length(x)
+        mu_hat = mean(x)
+        st_dev = std(x)
+        margin = ACS_margin(t, alpha) * st_dev
+        return Dict("l" => mu_hat - margin, "u" => mu_hat + margin)
+    end
 end
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,14 @@
-using ConfSets, Test, Statistics
+using ConfSets, Test, Statistics, Random
 
-function lower_upper(x, conf_set_fn, alpha)
-    lower = conf_set_fn(x, alpha)["l"]
-    upper = conf_set_fn(x, alpha)["u"]
+
+function lower_upper(x, conf_set_fn, alpha, sequential)
+    lower = conf_set_fn(x, alpha, sequential)["l"]
+    upper = conf_set_fn(x, alpha, sequential)["u"]
 
     return lower, upper
 end
 
+# Function to check if a vector is decreasing on average
 function is_decreasing_on_average(vector)
     # Calculate the differences between consecutive elements
     diffs = diff(vector)
@@ -19,11 +21,29 @@ function is_decreasing_on_average(vector)
 
     return is_decreasing
 end
+
+
 # Test the confidence intervals for the mean of a vector x, using the Central Limit Theorem.
+Random.seed!(1234)
 x = randn(1000)
 true_mean = 0
 
-l, u = lower_upper(x, clt_confidence_interval, 0.05)
+@test isapprox(clt_confidence_interval(x, 0.1, false)["u"], clt_confidence_interval(x, 0.1, true)["u"][end])
+@test isapprox(clt_confidence_interval(x, 0.1, false)["l"], clt_confidence_interval(x, 0.1, true)["l"][end])
+
+l, u = lower_upper(x, clt_confidence_interval, 0.05, false)
+@test l <= mean(x) <= u
+
+l, u = lower_upper(x, clt_confidence_interval, 0.05, true)
+@test l <= cumul_mean(x) <= u
+
+
+diff1 = abs.(l .- true_mean)
+diff2 = abs.(u .- true_mean)
+@test is_decreasing_on_average(diff1) == true
+@test is_decreasing_on_average(diff2) == true
+
+l, u = lower_upper(x, clt_confidence_interval, 0.01, true)
 @test l < cumul_mean(x) < u
 
 diff1 = abs.(l .- true_mean)
@@ -31,18 +51,11 @@ diff2 = abs.(u .- true_mean)
 @test is_decreasing_on_average(diff1) == true
 @test is_decreasing_on_average(diff2) == true
 
-l, u = lower_upper(x, clt_confidence_interval, 0.01)
-@test l < cumul_mean(x) < u
-
-diff1 = abs.(l .- true_mean)
-diff2 = abs.(u .- true_mean)
-@test is_decreasing_on_average(diff1) == true
-@test is_decreasing_on_average(diff2) == true
-
-@test clt_confidence_interval(x, 0.05) == clt_confidence_interval(x, 0.05)
+@test clt_confidence_interval(x, 0.05, true) == clt_confidence_interval(x, 0.05, true)
 
 
 # Test the confidence intervals for the mean of a vector x, using the Hoeffding inequality.
+Random.seed!(1234)
 a, b = 1, 10
 x = rand(a:b, 1000)
 
@@ -51,44 +64,51 @@ true_mean = (a + b) / 2
 range = b - a
 float_range = float(range)
 
-@test hoeff_confidence_interval(x, 0.1, range) == hoeff_confidence_interval(x, 0.1, float_range)
-@test hoeff_confidence_interval(x, 0.05, range) == hoeff_confidence_interval(x, 0.05, float_range)
-@test hoeff_confidence_interval(x, 0.01, range) == hoeff_confidence_interval(x, 0.01, float_range)
+@test hoeff_confidence_interval(x, 0.1, range, true) == hoeff_confidence_interval(x, 0.1, float_range, true)
+@test hoeff_confidence_interval(x, 0.05, range, true) == hoeff_confidence_interval(x, 0.05, float_range, true)
+@test hoeff_confidence_interval(x, 0.01, range, true) == hoeff_confidence_interval(x, 0.01, float_range, true)
 
-lower = hoeff_confidence_interval(x, 0.1, range)["l"]
-upper = hoeff_confidence_interval(x, 0.1, range)["u"]
+lower = hoeff_confidence_interval(x, 0.1, range, true)["l"]
+upper = hoeff_confidence_interval(x, 0.1, range, true)["u"]
+@test lower < cumul_mean(x) < upper
+
+lower = hoeff_confidence_interval(x, 0.1, range, false)["l"]
+upper = hoeff_confidence_interval(x, 0.1, range, false)["u"]
+@test lower < mean(x) < upper
+
+lower = hoeff_confidence_interval(x, 0.05, float_range, true)["l"]
+upper = hoeff_confidence_interval(x, 0.05, float_range, true)["u"]
 
 @test lower < cumul_mean(x) < upper
 
-lower = hoeff_confidence_interval(x, 0.05, float_range)["l"]
-upper = hoeff_confidence_interval(x, 0.05, float_range)["u"]
-
-@test lower < cumul_mean(x) < upper
-
-lower = hoeff_confidence_interval(x, 0.01, float_range)["l"]
-upper = hoeff_confidence_interval(x, 0.01, float_range)["u"]
+lower = hoeff_confidence_interval(x, 0.01, float_range, true)["l"]
+upper = hoeff_confidence_interval(x, 0.01, float_range, true)["u"]
 
 @test lower < cumul_mean(x) < upper
 
 
-diff1 = abs.(hoeff_confidence_interval(x, 0.05, range)["l"] .- true_mean)
-diff2 = abs.(hoeff_confidence_interval(x, 0.05, range)["u"] .- true_mean)
+diff1 = abs.(hoeff_confidence_interval(x, 0.05, range, true)["l"] .- true_mean)
+diff2 = abs.(hoeff_confidence_interval(x, 0.05, range, true)["u"] .- true_mean)
 @test is_decreasing_on_average(diff1) == true
 @test is_decreasing_on_average(diff2) == true
+
+@test isapprox(hoeff_confidence_interval(x, 0.1, range, false)["u"], hoeff_confidence_interval(x, 0.1, range, true)["u"][end])
+@test isapprox(hoeff_confidence_interval(x, 0.1, range, false)["l"], hoeff_confidence_interval(x, 0.1, range, true)["l"][end])
 
 
 # Test the confidence intervals for the mean of a vector x, using the Chebyshev inequality.
+Random.seed!(1234)
 x = randn(1000)
 true_mean = 0
 
-l, u = lower_upper(x, cheb_confidence_interval, 0.1)
+l, u = lower_upper(x, cheb_confidence_interval, 0.1, true)
 @test l < cumul_mean(x) < u
 diff1 = abs.(l .- true_mean)
 diff2 = abs.(u .- true_mean)
 @test is_decreasing_on_average(diff1) == true
 @test is_decreasing_on_average(diff2) == true
 
-l, u = lower_upper(x, cheb_confidence_interval, 0.05)
+l, u = lower_upper(x, cheb_confidence_interval, 0.05, true)
 @test l < cumul_mean(x) < u
 
 diff1 = abs.(l .- true_mean)
@@ -96,27 +116,40 @@ diff2 = abs.(u .- true_mean)
 @test is_decreasing_on_average(diff1) == true
 @test is_decreasing_on_average(diff2) == true
 
-@test cheb_confidence_interval(x, 0.05) == cheb_confidence_interval(x, 0.05)
-@test cheb_confidence_interval(x, 0.01) == cheb_confidence_interval(x, 0.01)
+@test cheb_confidence_interval(x, 0.05, true) == cheb_confidence_interval(x, 0.05, true)
+@test cheb_confidence_interval(x, 0.01, true) == cheb_confidence_interval(x, 0.01, true)
+
+@test isapprox(cheb_confidence_interval(x, 0.1, false)["u"], cheb_confidence_interval(x, 0.1, true)["u"][end])
+@test isapprox(cheb_confidence_interval(x, 0.1, false)["l"], cheb_confidence_interval(x, 0.1, true)["l"][end])
+
+l, u = lower_upper(x, cheb_confidence_interval, 0.05, false)
+@test l <= mean(x) <= u
 
 
 # Test the confidence sets for the mean of a vector x, using the Asymptotic Confidence Sequence.
+Random.seed!(1234)
 x = randn(1000)
 true_mean = 0
 
-l, u = lower_upper(x, Asymp_conf_seq, 0.05)
+l, u = lower_upper(x, Asymp_conf_seq, 0.05, true)
 @test l <= cumul_mean(x) <= u
 diff1 = abs.(l .- true_mean)
 diff2 = abs.(u .- true_mean)
 @test is_decreasing_on_average(diff1) == true
 @test is_decreasing_on_average(diff2) == true
 
-l, u = lower_upper(x, Asymp_conf_seq, 0.01)
+l, u = lower_upper(x, Asymp_conf_seq, 0.01, true)
 @test l <= cumul_mean(x) <= u
 diff1 = abs.(l .- true_mean)
 diff2 = abs.(u .- true_mean)
 @test is_decreasing_on_average(diff1) == true
 @test is_decreasing_on_average(diff2) == true
 
-@test Asymp_conf_seq(x, 0.05) == Asymp_conf_seq(x, 0.05)
-@test Asymp_conf_seq(x, 0.01) == Asymp_conf_seq(x, 0.01)
+@test Asymp_conf_seq(x, 0.05, true) == Asymp_conf_seq(x, 0.05, true)
+@test Asymp_conf_seq(x, 0.01, true) == Asymp_conf_seq(x, 0.01, true)
+
+@test isapprox(Asymp_conf_seq(x, 0.1, false)["u"], Asymp_conf_seq(x, 0.1, true)["u"][end])
+@test isapprox(Asymp_conf_seq(x, 0.1, false)["l"], Asymp_conf_seq(x, 0.1, true)["l"][end])
+
+l, u = lower_upper(x, Asymp_conf_seq, 0.05, false)
+@test l <= mean(x) <= u
